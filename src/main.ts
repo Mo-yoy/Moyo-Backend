05cb0e16b224cbf6b14abacda6f0c888fe9e564c
jungwoo3490 <jungwoo3490@naver.com>
2024-12-24 02:44:36 +0900

    chore: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+  const allowedOrigins = process.env.ALLOWED_ORIGINS;
 
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT;
   await app.listen(port);
 }
 bootstrap();
